6c7ee6d504616cecdfb483c3f2abc1d98e1abac5
blloop <42186044+blloop@users.noreply.github.com>
2022-07-11 21:21:33 -0700

    Line fixes

--- source/src/Events.tsx
+++ source/src/Events.tsx
@@ -11,9 +11,11 @@ class Events extends Component<Props, State> {
 
     let output:any[] = [];
     for (let i = 0; i < this.props.eventList.length; i++) {
-      output.push(<div className='event'> List Item: {this.props.eventList[i]} 
-        <input defaultValue={"Description"}></input>
-      </div>);
+      output.push(
+        <div className='event'> List Item: {this.props.eventList[i]} 
+          <input defaultValue={"Description"}></input>
+        </div>
+      );
     }
     return (
       <div>        
